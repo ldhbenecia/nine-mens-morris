29069021f7caf313599a11dab8c9cb43a4f01521
DongHyeok Lim <jh07050@gmail.com>
2024-04-04 16:10:15 +0900

    feat: Kakao로부터 해당 유저의 정보를 받은 후 DB에 회원등록

---
+++ server/src/main/java/com/ninemensmorris/auth/service/CustomOAuth2UserService.java
@@ -0,0 +1,65 @@
+package com.ninemensmorris.auth.service;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.ninemensmorris.user.domain.User;
+import com.ninemensmorris.user.repository.UserRepository;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.security.oauth2.client.userinfo.DefaultOAuth2UserService;
+import org.springframework.security.oauth2.client.userinfo.OAuth2UserRequest;
+import org.springframework.security.oauth2.core.OAuth2AuthenticationException;
+import org.springframework.security.oauth2.core.user.OAuth2User;
+import org.springframework.stereotype.Service;
+
+import java.util.Map;
+
+@Service
+@Slf4j
+@RequiredArgsConstructor
+public class CustomOAuth2UserService extends DefaultOAuth2UserService {
+
+    private final UserRepository userRepository;
+
+    @Override
+    public OAuth2User loadUser(OAuth2UserRequest userRequest) throws OAuth2AuthenticationException {
+        OAuth2User oAuth2User = super.loadUser(userRequest);
+        String oauthClientName = userRequest.getClientRegistration().getClientName();
+
+        try {
+            System.out.println(new ObjectMapper().writeValueAsString(oAuth2User.getAttributes()));
+        } catch (Exception exception) {
+            log.error("Failed to oAuth2User: {}", exception.getMessage());
+        }
+
+        if (oauthClientName.equals("kakao")) {
+            String idString = oAuth2User.getAttribute("id").toString();
+            Long id = Long.parseLong(idString);
+            User user = userRepository.findByProviderId(id);
+            if (user == null) {
+                user = createUserFromOAuth2User(oAuth2User, id);
+            }
+        }
+
+        return oAuth2User;
+    }
+
+    private User createUserFromOAuth2User(OAuth2User oAuth2User, Long userId) {
+
+        Map<String, Object> properties = oAuth2User.getAttribute("properties");
+        Map<String, Object> account = oAuth2User.getAttribute("kakao_account");
+
+        String nickname = (String) properties.get("nickname") ;
+        String email = (String) account.get("email");
+        String profileImg = (String) properties.get("profile_image");
+
+        User user = User.builder()
+                .providerId(userId)
+                .email(email)
+                .nickname(nickname)
+                .imageUrl(profileImg)
+                .score(0)
+                .build();
+
+        return userRepository.save(user);
+    }
+}
