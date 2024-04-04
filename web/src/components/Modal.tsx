233ec4bba72936e955f2b970231cf5c6ed7ced61
Jiseung <js43o@naver.com>
2024-04-04 23:52:40 +0900

    💄 카카오 로그인 버튼 및 로그인 모달 추가

---
+++ web/src/components/Modal.tsx
@@ -0,0 +1,13 @@
+type ModalProps = {
+  children: React.ReactElement;
+};
+
+export function Modal({ children }: ModalProps) {
+  return (
+    <div className="fixed flex h-full w-full items-center justify-center bg-[rgba(0,0,0,0.2)]">
+      <div className="flex min-w-[22.5rem] flex-col items-center gap-6 rounded-2xl border border-gray-300 bg-white p-8 shadow-lg">
+        {children}
+      </div>
+    </div>
+  );
+}
--- web/src/pages/Main/index.tsx
+++ web/src/pages/Main/index.tsx
@@ -1,8 +1,28 @@
+import { useState } from 'react';
 import { Button } from '~/components/Button';
+import { KakaoButton } from '~/components/KakaoButton';
+import { Modal } from '~/components/Modal';
 
 export function MainPage() {
+  const [showModal, setShowModal] = useState(false);
+
   return (
     <main className="flex h-full flex-col items-center justify-center gap-12">
+      {showModal && (
+        <Modal>
+          <>
+            <div className="text-lg font-semibold">로그인이 필요합니다.</div>
+            <div className="flex w-full flex-col gap-4">
+              <KakaoButton />
+              <Button
+                theme="secondary"
+                text="취소"
+                onClick={() => setShowModal(false)}
+              />
+            </div>
+          </>
+        </Modal>
+      )}
       <div className="flex flex-col items-center gap-2">
         <h1 className="font-title text-5xl">Nine Men&apos;s Morris</h1>
         <h2 className="text-2xl font-light tracking-[0.8rem] text-gray-500">
@@ -10,7 +30,7 @@ export function MainPage() {
         </h2>
       </div>
       <div className="flex w-60 flex-col gap-4">
-        <Button text="게임 시작" onClick={() => {}} />
+        <Button text="게임 시작" onClick={() => setShowModal(true)} />
         <Button theme="secondary" text="랭킹 보기" onClick={() => {}} />
       </div>
     </main>
