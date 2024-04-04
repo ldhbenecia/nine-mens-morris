fd81ae4128450eca6c14ce03d964f92ae97f5c33
Jiseung <js43o@naver.com>
2024-04-05 00:06:14 +0900

    💄 모달 애니메이션 적용

--- web/src/components/Modal.tsx
+++ web/src/components/Modal.tsx
@@ -4,8 +4,8 @@ type ModalProps = {
 
 export function Modal({ children }: ModalProps) {
   return (
-    <div className="fixed flex h-full w-full items-center justify-center bg-[rgba(0,0,0,0.2)]">
-      <div className="flex min-w-[22.5rem] flex-col items-center gap-6 rounded-2xl border border-gray-300 bg-white p-8 shadow-lg">
+    <div className="animate-modal-background fixed flex h-full w-full items-center justify-center bg-[rgba(0,0,0,0.2)]">
+      <div className="animate-modal flex min-w-[22.5rem] flex-col items-center gap-6 rounded-2xl border border-gray-300 bg-white p-8 shadow-lg">
         {children}
       </div>
     </div>
--- web/tailwind.config.js
+++ web/tailwind.config.js
@@ -8,6 +8,20 @@ export default {
         title: 'Kumbh Sans',
         phase: 'Big Shoulders Text',
       },
+      keyframes: {
+        'fade-in': {
+          '0%': { opacity: 0 },
+          '100%': { opacity: 1 },
+        },
+        'move-up': {
+          '0%': { transform: 'translateY(5%)' },
+          '100%': { transform: 'translateY(0%)' },
+        },
+      },
+      animation: {
+        'modal-background': 'fade-in 0.25s ease forwards',
+        modal: 'move-up 0.25s ease forwards',
+      },
     },
   },
   plugins: [],
