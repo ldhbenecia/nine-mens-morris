2b3aac4a6cbf491796455f12d2e1fa70a6c1eb65
DongHyeok Lim <jh07050@gmail.com>
2024-04-04 16:08:40 +0900

    refactor: Kakao ProviderId Key값으로 사용

--- server/src/main/java/com/ninemensmorris/user/domain/User.java
+++ server/src/main/java/com/ninemensmorris/user/domain/User.java
@@ -10,9 +10,8 @@ import lombok.*;
 @AllArgsConstructor
 public class User {
 
-    @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
-    @Column(name = "user_id")
-    private Long id;
+    @Id
+    private Long providerId;
 
     private String email;
     private String nickname;
