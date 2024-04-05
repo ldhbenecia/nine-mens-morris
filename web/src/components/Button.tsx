75ec3cb5a758049ab44e93696e9422ad796407dd
Jiseung <js43o@naver.com>
2024-04-05 11:39:39 +0900

    🏷️ 버튼 컴포넌트 props 추가

--- web/src/components/Button.tsx
+++ web/src/components/Button.tsx
@@ -1,19 +1,23 @@
 type ButtonProps = {
-  theme?: 'primary' | 'secondary';
-  icon?: React.ReactNode;
   text: string;
   onClick: React.MouseEventHandler<HTMLButtonElement>;
+  theme?: 'primary' | 'secondary';
+  icon?: React.ReactNode;
+  slim?: boolean;
+  fullWidth?: boolean;
 };
 
 export function Button({
-  theme = 'primary',
-  icon,
   text,
   onClick,
+  theme = 'primary',
+  icon,
+  slim = false,
+  fullWidth = false,
 }: ButtonProps) {
   return (
     <button
-      className={`flex w-full cursor-default justify-center gap-1 rounded-lg p-4 text-lg font-semibold ${theme === 'primary' ? 'border border-gray-800 bg-gray-800 text-white hover:bg-gray-700 active:bg-gray-900' : 'border border-gray-300 bg-gray-50 text-black hover:bg-white active:bg-gray-100'}`}
+      className={`flex cursor-default justify-center gap-1 rounded-lg text-lg font-semibold ${fullWidth ? 'w-full' : ''} ${slim ? 'px-4 py-2' : 'p-4'} ${theme === 'primary' ? 'border border-gray-800 bg-gray-800 text-white hover:bg-gray-700 active:bg-gray-900' : 'border border-gray-300 bg-gray-50 text-black hover:bg-white active:bg-gray-100'}`}
       onClick={onClick}
     >
       {icon}
