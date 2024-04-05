d188bc59df1bd7115c34276b1251fab0312d6477
DongHyeok Lim <jh07050@gmail.com>
2024-04-05 18:04:29 +0900

    feat: Token을 Header가 아닌 Cookie에서 파싱

--- server/src/main/java/com/ninemensmorris/security/filter/JwtAuthenticationFilter.java
+++ server/src/main/java/com/ninemensmorris/security/filter/JwtAuthenticationFilter.java
@@ -5,20 +5,24 @@ import com.ninemensmorris.user.domain.User;
 import com.ninemensmorris.user.repository.UserRepository;
 import jakarta.servlet.FilterChain;
 import jakarta.servlet.ServletException;
+import jakarta.servlet.http.Cookie;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.security.authentication.AbstractAuthenticationToken;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.core.context.SecurityContext;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.web.authentication.WebAuthenticationDetailsSource;
 import org.springframework.stereotype.Component;
-import org.springframework.util.StringUtils;
 import org.springframework.web.filter.OncePerRequestFilter;
 
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
 
 @Component
 @RequiredArgsConstructor
@@ -33,8 +37,7 @@ public class JwtAuthenticationFilter extends OncePerRequestFilter {
             throws ServletException, IOException {
 
         try {
-
-            String token = parseBearerToken(request);
+            String token = parseTokenFromCookie(request);
             if (token == null) {
                 filterChain.doFilter(request, response);
                 return;
@@ -48,23 +51,19 @@ public class JwtAuthenticationFilter extends OncePerRequestFilter {
             }
 
             Long userId = jwtProvider.extractSubject(token);
-            User user = userRepository.findById(userId).orElse(null);
-            if (user == null) {
-                log.error("User not found for user id: {}", userId);
-                return;
-            }
+            User user = userRepository.findByUserId(userId);
+
+            String role = user.getRole();
+            List<GrantedAuthority> authorities = new ArrayList<>();
+            authorities.add(new SimpleGrantedAuthority(role));
 
             SecurityContext securityContext = SecurityContextHolder.createEmptyContext();
 
-            // 사용자의 이메일을 사용하여 인증 토큰 생성
             AbstractAuthenticationToken authenticationToken =
-                    new UsernamePasswordAuthenticationToken(userId, null);
+                    new UsernamePasswordAuthenticationToken(user.getNickname(), null, authorities);
             authenticationToken.setDetails(new WebAuthenticationDetailsSource().buildDetails(request));
 
-            // 생성된 인증 토큰을 보안 컨텍스트에 설정
             securityContext.setAuthentication(authenticationToken);
-
-            // 설정된 보안 컨텍스트를 SecurityContextHolder에 설정
             SecurityContextHolder.setContext(securityContext);
 
         } catch (Exception exception) {
@@ -74,15 +73,15 @@ public class JwtAuthenticationFilter extends OncePerRequestFilter {
         filterChain.doFilter(request, response);
     }
 
-    private String parseBearerToken(HttpServletRequest request) {
-
-        String authorization = request.getHeader("Authorization");
-        boolean hasAuthorization = StringUtils.hasText(authorization);
-        if (!hasAuthorization) return null;
-
-        boolean isBearer = authorization.startsWith("Bearer ");
-        if (!isBearer) return null;
-
-        return authorization.substring(7);
+    private String parseTokenFromCookie(HttpServletRequest request) {
+        Cookie[] cookies = request.getCookies();
+        if (cookies != null) {
+            for (Cookie cookie : cookies) {
+                if ("access_token".equals(cookie.getName())) {
+                    return cookie.getValue();
+                }
+            }
+        }
+        return null;
     }
 }
