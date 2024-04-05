e4cf3734cd9eb7733faede3d23d5440a55c413fa
DongHyeok Lim <jh07050@gmail.com>
2024-04-05 13:55:33 +0900

    feat: User - GameRoom Entity

--- server/src/main/java/com/ninemensmorris/user/domain/User.java
+++ server/src/main/java/com/ninemensmorris/user/domain/User.java
@@ -1,20 +1,27 @@
 package com.ninemensmorris.user.domain;
 
+import com.ninemensmorris.game.domain.GameRoom;
 import jakarta.persistence.*;
 import lombok.*;
 
 @Entity
 @Getter
+@Setter
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
 @Builder
 @AllArgsConstructor
 public class User {
 
     @Id
+    @Column(name = "user_id")
     private Long userId;
 
     private String email;
     private String nickname;
     private String imageUrl;
     private int score;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "game_room_id")
+    private GameRoom gameRoom;
 }
