28d10619501c6f52a2b6e2c954c0794ecef2bd3a
DongHyeok Lim <jh07050@gmail.com>
2024-04-05 13:54:52 +0900

    feat: WebSocketCongfig Setting

---
+++ server/src/main/java/com/ninemensmorris/config/WebSocketConfig.java
@@ -0,0 +1,25 @@
+package com.ninemensmorris.config;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.messaging.simp.config.MessageBrokerRegistry;
+import org.springframework.web.socket.config.annotation.EnableWebSocketMessageBroker;
+import org.springframework.web.socket.config.annotation.StompEndpointRegistry;
+import org.springframework.web.socket.config.annotation.WebSocketMessageBrokerConfigurer;
+
+@Configuration
+@RequiredArgsConstructor
+@EnableWebSocketMessageBroker
+public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
+
+    @Override
+    public void configureMessageBroker(MessageBrokerRegistry config) {
+        config.enableSimpleBroker("/morris/game-room");
+        config.setApplicationDestinationPrefixes("/morris");
+    }
+
+    @Override
+    public void registerStompEndpoints(StompEndpointRegistry registry) {
+        registry.addEndpoint("/ninemensmorris-ws").withSockJS();
+    }
+}
