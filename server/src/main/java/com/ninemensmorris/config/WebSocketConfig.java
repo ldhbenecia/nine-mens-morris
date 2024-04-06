220c3105da049991c9a8b267b8be625fbf528847
DongHyeok Lim <jh07050@gmail.com>
2024-04-06 21:07:49 +0900

    feat: WebSocket STOMP로 변경

--- server/src/main/java/com/ninemensmorris/config/WebSocketConfig.java
+++ server/src/main/java/com/ninemensmorris/config/WebSocketConfig.java
@@ -1,6 +1,5 @@
 package com.ninemensmorris.config;
 
-import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.messaging.simp.config.MessageBrokerRegistry;
 import org.springframework.web.socket.config.annotation.EnableWebSocketMessageBroker;
@@ -8,18 +7,17 @@ import org.springframework.web.socket.config.annotation.StompEndpointRegistry;
 import org.springframework.web.socket.config.annotation.WebSocketMessageBrokerConfigurer;
 
 @Configuration
-@RequiredArgsConstructor
 @EnableWebSocketMessageBroker
 public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
 
     @Override
-    public void configureMessageBroker(MessageBrokerRegistry config) {
-        config.enableSimpleBroker("/morris/game-room");
-        config.setApplicationDestinationPrefixes("/morris");
+    public void configureMessageBroker(MessageBrokerRegistry registry) {
+        registry.enableSimpleBroker("/topic");
+        registry.setApplicationDestinationPrefixes("/app");
     }
 
     @Override
     public void registerStompEndpoints(StompEndpointRegistry registry) {
-        registry.addEndpoint("/ninemensmorris-ws").withSockJS();
+        registry.addEndpoint("/game-websocket").withSockJS();
     }
 }
