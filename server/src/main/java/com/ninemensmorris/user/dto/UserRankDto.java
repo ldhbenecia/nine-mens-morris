f798147269a15791bfc18e1a70634bccaf6c434d
DongHyeok Lim <jh07050@gmail.com>
2024-04-10 19:01:08 +0900

    feat: 유저 프로필 이미지 추가 반환

--- server/src/main/java/com/ninemensmorris/user/dto/UserRankDto.java
+++ server/src/main/java/com/ninemensmorris/user/dto/UserRankDto.java
@@ -8,9 +8,11 @@ public class UserRankDto {
 
     private String nickname;
     private int score;
+    private String imageUrl;
 
     public UserRankDto(User user) {
         this.nickname = user.getNickname();
         this.score = user.getScore();
+        this.imageUrl = user.getImageUrl();
     }
 }
