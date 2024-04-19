e4c74744e8c6cf647000d6d01bc323ab413d30c1
Jiseung <js43o@naver.com>
2024-04-19 17:44:42 +0900

    🐛 배포 전용 카카오 로그인 URL 변경

--- web/src/components/KakaoButton.tsx
+++ web/src/components/KakaoButton.tsx
@@ -3,10 +3,12 @@ import Kakao from '~/assets/icons/kakao.svg?react';
 export function KakaoButton() {
   return (
     <button
-      className="flex w-full cursor-default justify-center gap-2 rounded-lg bg-[#FEE500] p-3 font-semibold"
+      className="flex w-full cursor-pointer justify-center gap-2 rounded-lg bg-[#FEE500] p-3 font-semibold"
       onClick={() =>
         (window.location.href =
-          'http://localhost:8080/oauth2/authorization/kakao')
+          import.meta.env.MODE === 'development'
+            ? 'http://localhost:8080/oauth2/authorization/kakao'
+            : 'https://api.ninemensmorris.site:8080/oauth2/authorization/kakao')
       }
     >
       <Kakao />
