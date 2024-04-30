6d2c11205c733b29e4914eec390a87a27b719c76
Donghyeok Lim <jh07050@gmail.com>
2024-04-30 21:47:47 +0900

    Delete mysql-init.d directory

--- server/mysql-init.d/init.sql
+++
@@ -1,5 +0,0 @@
-CREATE USER 'user'@'localhost' IDENTIFIED BY 'password';
-CREATE USER 'user'@'%' IDENTIFIED BY 'password';
-
-GRANT ALL PRIVILEGES ON *.* TO 'user'@'localhost';
-GRANT ALL PRIVILEGES ON *.* TO 'user'@'%';
